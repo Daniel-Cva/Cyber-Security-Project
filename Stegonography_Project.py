90230e99d7338884347f9134d3c9fca17d5609e6
Daniel Jabaraj V <danieljabaraj218@gmail.com>
2025-03-08 12:49:35 +0530

    Update Stegonography_Project.py
    Change normal encryption to AES Advanced Encryption Standard Algorithm

--- Stegonography_Project.py
+++ Stegonography_Project.py
@@ -1,11 +1,34 @@
 import cv2
 import os
 import string
+from Crypto.Cipher import AES
+import base64
+import hashlib
+
+def pad_message(msg):
+    # Padding to ensure the message is a multiple of 16 bytes
+    while len(msg) % 16 != 0:
+        msg += ' '
+    return msg
+
+def encrypt_message(msg, password):
+    key = hashlib.sha256(password.encode()).digest()
+    cipher = AES.new(key, AES.MODE_ECB)
+    encrypted_message = base64.b64encode(cipher.encrypt(pad_message(msg).encode()))
+    return encrypted_message.decode('utf-8')
+
+def decrypt_message(encrypted_message, password):
+    key = hashlib.sha256(password.encode()).digest()
+    cipher = AES.new(key, AES.MODE_ECB)
+    decrypted_message = cipher.decrypt(base64.b64decode(encrypted_message)).decode('utf-8').strip()
+    return decrypted_message
 
 img = cv2.imread("SecretX.jpg") # Replace with the correct image path
 
 msg = input("Enter secret message: ")
 password = input("Enter a passcode: ")
+
+encrypted_msg = encrypt_message(msg, password)
 
 d = {}
 c = {}
@@ -18,8 +41,8 @@ m = 0
 n = 0
 z = 0
 
-for i in range(len(msg)):
-    img[n, m, z] = d[msg[i]]
+for i in range(len(encrypted_msg)):
+    img[n, m, z] = d[encrypted_msg[i]]
     n = n + 1
     m = m + 1
     z = (z + 1) % 3
@@ -34,11 +57,12 @@ z = 0
 
 pas = input("Enter passcode for Decryption: ")
 if password == pas:
-    for i in range(len(msg)):
+    for i in range(len(encrypted_msg)):
         message = message + c[img[n, m, z]]
         n = n + 1
         m = m + 1
         z = (z + 1) % 3
-    print("Decrypted message:", message)
+    decrypted_msg = decrypt_message(message, password)
+    print("Decrypted message:", decrypted_msg)
 else:
     print("Your Passcode Is Wrong 👻")
